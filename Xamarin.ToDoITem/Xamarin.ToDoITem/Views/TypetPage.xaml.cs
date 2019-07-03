e3656ba72359f9706bea259b2e6cdea4739dcf8e
Georg Dimopoulos <georgdimopoulos@gmail.com>
2019-07-03 17:30:14 +0300

    Repare Pickers in TYPET Page

--- Xamarin.ToDoITem/Xamarin.ToDoITem/Views/TypetPage.xaml.cs
+++ Xamarin.ToDoITem/Xamarin.ToDoITem/Views/TypetPage.xaml.cs
@@ -27,6 +27,15 @@ namespace MyItems.Views
             try
             {
                 InitializeComponent();
+                TodoPicker.Items.Clear();
+                TodoPicker.Items.Add("Διαγραφή");
+                TodoPicker.Items.Add("Αλλαγή");
+                KidsPicker.Items.Clear();
+                KidsPicker.Items.Add("Διαγραφή");
+                KidsPicker.Items.Add("Αλλαγή");
+                StaffPicker.Items.Clear();
+                StaffPicker.Items.Add("Διαγραφή");
+                StaffPicker.Items.Add("Αλλαγή");
                 NavigationPage.SetHasNavigationBar(this, true);
                 NavigationPage.SetHasBackButton(this, false);                
             }
@@ -55,71 +64,39 @@ namespace MyItems.Views
             }
         }
 
-        private void TodoDutiesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("TodoDutiesListView_OnItemTapped", ex.Message, "OK");
-            }
-        }
-
         private void TodoDutiesListView_OnUnfocused(object sender, FocusEventArgs e)
         {
             try
             {
-
+                TodoDutiesListView.IsVisible = false;
+                TodoDutiesListView.Unfocus();
+                TodoDutiesListView.SelectedItem = null;
             }
             catch (Exception ex)
             {
                 DisplayAlert("TodoDutiesListView_OnUnfocused", ex.Message, "OK");
             }
         }
-
-        private void KidsDutiesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("KidsDutiesListView_OnItemTapped", ex.Message, "OK");
-            }
-        }
-
         private void KidsDutiesListView_OnUnfocused(object sender, FocusEventArgs e)
         {
             try
             {
-
+                KidsDutiesListView.IsVisible = false;
+                KidsDutiesListView.Unfocus();
+                KidsDutiesListView.SelectedItem = null;
             }
             catch (Exception ex)
             {
                 DisplayAlert("KidsDutiesListView_OnUnfocused", ex.Message, "OK");
             }
         }
-
-        private void StaffDutiesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                DisplayAlert("StaffDutiesListView_OnItemTapped", ex.Message, "OK");
-            }
-        }
-
         private void StaffDutiesListView_OnUnfocused(object sender, FocusEventArgs e)
         {
             try
             {
-
+                StaffDutiesListView.IsVisible = false;
+                StaffDutiesListView.Unfocus();
+                StaffDutiesListView.SelectedItem = null;
             }
             catch (Exception ex)
             {
@@ -127,6 +104,49 @@ namespace MyItems.Views
             }
         }
 
+        private void TodoDutiesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            try
+            {
+                var task = (Task)TodoDutiesListView.SelectedItem;
+                currentTask = task;
+                TodoPicker.IsVisible = true;
+                TodoPicker.Focus();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("TodoDutiesListView_OnItemTapped", ex.Message, "OK");
+            }
+        }
+        private void KidsDutiesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            try
+            {
+                var task = (Task)KidsDutiesListView.SelectedItem;
+                currentTask = task;
+                KidsPicker.IsVisible = true;
+                KidsPicker.Focus();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("KidsDutiesListView_OnItemTapped", ex.Message, "OK");
+            }
+        }
+        private void StaffDutiesListView_OnItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            try
+            {
+                var task = (Task)StaffDutiesListView.SelectedItem;
+                currentTask = task;
+                StaffPicker.IsVisible = true;
+                StaffPicker.Focus();
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("StaffDutiesListView_OnItemTapped", ex.Message, "OK");
+            }
+        }
+        
         private async void ToDoItemButton_OnClicked(object sender, EventArgs e)
         {
             try
@@ -138,7 +158,7 @@ namespace MyItems.Views
                 }
                 var task = new Task
                 {
-                    Text = StaffItemEntry.Text,
+                    Text = ToDoItemEntry.Text,
                     Type = 33
                 };
                 UserDialogs.Instance.ShowLoading();
@@ -194,7 +214,7 @@ namespace MyItems.Views
                 }
                 var task = new Task
                 {
-                    Text = StaffItemEntry.Text,
+                    Text = KidItemEntry.Text,
                     Type = 31
                 };
                 UserDialogs.Instance.ShowLoading();
@@ -208,6 +228,119 @@ namespace MyItems.Views
             catch (Exception ex)
             {
                await DisplayAlert("KidItemButton_OnClicked", ex.Message, "OK");
+            }
+        }
+        
+        private async void TodoPicker_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TodoPicker.SelectedIndex == 0)
+                {
+                    UserDialogs.Instance.ShowLoading();
+                    myWholeList.Remove(currentTask);
+                    await App.ItemController.DeleteTask(currentTask);
+                    TodoDutiesListView.ItemsSource = myWholeList.Where(x => x.Type.Equals(33)); ;
+                    UserDialogs.Instance.HideLoading();
+                    await DisplayAlert(null, "Επιτυχής Διαγραφή!", "OK");
+                }
+                else if (TodoPicker.SelectedIndex == 1)
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("TodoPicker_OnSelectedIndexChanged", ex.Message, "OK");
+            }
+        }
+
+        private async void KidsPicker_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (KidsPicker.SelectedIndex == 0)
+                {
+                    UserDialogs.Instance.ShowLoading();
+                    myWholeList.Remove(currentTask);
+                    await App.ItemController.DeleteTask(currentTask);
+                    KidsDutiesListView.ItemsSource = myWholeList.Where(x => x.Type.Equals(31)); ;
+                    UserDialogs.Instance.HideLoading();
+                    await DisplayAlert(null, "Επιτυχής Διαγραφή!", "OK");
+                }
+                else if (KidsPicker.SelectedIndex == 1)
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("KidsPicker_OnSelectedIndexChanged", ex.Message, "OK");
+            }
+        }
+
+        private async void StuffPicker_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (StaffPicker.SelectedIndex == 0)
+                {
+                    UserDialogs.Instance.ShowLoading();
+                    myWholeList.Remove(currentTask);
+                    await App.ItemController.DeleteTask(currentTask);
+                    StaffDutiesListView.ItemsSource = myWholeList.Where(x => x.Type.Equals(32)); ;
+                    UserDialogs.Instance.HideLoading();
+                    await DisplayAlert(null, "Επιτυχής Διαγραφή!", "OK");
+                }
+                else if (StaffPicker.SelectedIndex == 1)
+                {
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("StuffPicker_OnSelectedIndexChanged", ex.Message, "OK");
+            }
+        }
+
+        private async void TodoPicker_OnUnfocused(object sender, FocusEventArgs e)
+        {
+            try
+            {
+                TodoDutiesListView.SelectedItem = null;
+                TodoPicker.SelectedItem = null;
+                TodoPicker.IsVisible = false;
+                TodoPicker.Unfocus();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("TodoPicker_OnUnfocused", ex.Message, "OK");
+            }
+        }
+        private async void StuffPicker_OnUnfocused(object sender, FocusEventArgs e)
+        {
+            try
+            {
+                StaffPicker.SelectedItem = null;
+                StaffDutiesListView.SelectedItem = null;
+                StaffPicker.IsVisible = false;
+                StaffPicker.Unfocus();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("StuffPicker_OnUnfocused", ex.Message, "OK");
+            }
+        }
+        private async void KidsPicker_OnUnfocused(object sender, FocusEventArgs e)
+        {
+            try
+            {
+                KidsPicker.SelectedItem = null;
+                KidsDutiesListView.SelectedItem = null;
+                KidsPicker.IsVisible = false;
+                KidsPicker.Unfocus();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("KidsPicker_OnUnfocused", ex.Message, "OK");
             }
         }
     }
